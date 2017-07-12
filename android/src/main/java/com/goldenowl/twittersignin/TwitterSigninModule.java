b07d9ce42a4f841b6b5cdcde8fdab1da9f6fcd01
Maksym Rakus <maksym.rakus@rgand.com>
2017-07-12 09:53:49 +0300

    'update'

--- android/src/main/java/com/goldenowl/twittersignin/TwitterSigninModule.java
+++ android/src/main/java/com/goldenowl/twittersignin/TwitterSigninModule.java
@@ -130,7 +130,11 @@ public class TwitterSigninModule extends ReactContextBaseJavaModule implements A
     }
 
     @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+    public void onNewIntent(Intent intent) {
+    }
+
+    @Override
+    public void onActivityResult(Activity currentActivity, int requestCode, int resultCode, Intent data) {
         if (twitterAuthClient != null && twitterAuthClient.getRequestCode() == requestCode) {
             twitterAuthClient.onActivityResult(requestCode, resultCode, data);
         }
